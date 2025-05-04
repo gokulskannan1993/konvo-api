3ae31e2849ed20c9c6dc439d48f1c69149779528
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-04 12:27:12 +0100

    fix: change package type to module and update server.js to use ES modules

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-const express = require('express');
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config(); // Load environment variables from .env file
+
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000; // Use PORT from .env or default to 5000
 
 // Middleware
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get('/api/auth/signup', (req, res) => {
+    res.send('Signup route');
 });
+
+app.get('/api/auth/login', (req, res) => {
+    res.send('Login route');
+});
+
+
+app.get('/api/auth/logout', (req, res) => {
+    res.send('logout route');
+});
+
+
 
 // Start the server
 app.listen(PORT, () => {
